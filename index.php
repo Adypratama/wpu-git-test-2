f9575aa7b25ddf09561a6a26db8ed0abde9b985d
Adypratama <pady889@gmail.com>
2022-06-14 11:16:07 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Coba Git Remote 2</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas error aspernatur minus impedit ipsum dolore earum adipisci, aut dolorem quam illo repudiandae fuga fugit tenetur, dicta nam similique a odio?</p>
 </body>
 </html>
